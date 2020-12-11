d5286cbe027abc79d655748fec71f6c3148b3d99
ruslanchampion <ruslan.timbakov@gmail.com>
2020-12-11 15:55:16 +0800

    feat: lets date

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(val) {
+  return new Date(val);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(val) {
+  return new Date(val);
 }
 
 
@@ -53,8 +53,21 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+// 1.Если год делится на 4 без остатка, перейдите на шаг 2.
+// В противном случае перейдите к выполнению действия 5.
+// 2.Если год делится на 100 без остатка, перейдите на шаг 3.
+// В противном случае перейдите к выполнению действия 4.
+// 3.Если год делится на 400 без остатка, перейдите на шаг 4.
+// В противном случае перейдите к выполнению действия 5.
+// 4.Год високосный (366 дней).
+// 5.Год не високосный год (365 дней).
+
+function isLeapYear(date) {
+  const currentYear = date.getFullYear();
+  if (currentYear % 4) return false;
+  if (currentYear % 100) return true;
+  if (currentYear % 400) return false;
+  return true;
 }
 
 
@@ -73,8 +86,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  return new Date(endDate - startDate).toISOString().slice(11, -1);
 }
 
 
@@ -94,8 +107,9 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const angleRad = Math.abs(0.5 * (60 * (date.getUTCHours() % 12) - 11 * date.getUTCMinutes()));
+  return ((angleRad > 180 ? 360 - angleRad : angleRad) * Math.PI) / 180;
 }
 
 
